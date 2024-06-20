84972a6c52d66582f8c3cdb687a37ab0dcdd00c0
Saksham Mittal <saksham2005mittal@gmail.com>
2024-06-20 23:06:20 +0530

    Completed File encrypt/decrypt integration with python

--- src/python_file_encrypt_decrypt.c
+++ src/python_file_encrypt_decrypt.c
@@ -4,20 +4,10 @@
 // and python can import them via the ctypes module (CDLL)
 
 #include <sodium.h>
+#include <stdio.h>
 #include "dfs/fileencrypt.h"
 #include "dfs/filedecrypt.h"
 #include "dfspython/python_file_encrypt_decrypt.h"
-
-// It is a lot easier to work with sqlite in python so for the first prototype, SQLite
-// operations would be handled in python (after that in C/C++). This struct is used to
-// give whether success or not (via status code) and the generated key and nonce (to store
-// them in SQLite). This SQLite db remains on the client machine ONLY.
-
-struct keyNoncePair_secretbox {
-    int statusCode;
-    unsigned char key[crypto_secretbox_KEYBYTES];
-    unsigned char nonce[crypto_secretbox_NONCEBYTES];
-};
 
 struct keyNoncePair_secretbox perform_dfs_divideFileIntoChunks_encryptedSecretbox(
     const char* inputFilePath, const char* inputFileName, const int numChunks,
@@ -39,14 +29,14 @@ struct keyNoncePair_secretbox perform_dfs_divideFileIntoChunks_encryptedSecretbo
     // Call the actual function
     resultantPair.statusCode = dfs_divideFileIntoChunks_encryptedSecretbox(
         inputFilePath, inputFileName, numChunks, headerToken, headerSizeInBytes,
-        outputDirPath, key, nonce);
+        outputDirPath, resultantPair.key, resultantPair.nonce);
 
     return resultantPair;
 }
 
 int perform_dfs_decryptAndReconstructFile_secretbox(
-    struct keyNoncePair_secretbox usePair, const char* outputFilePath, const int headerSizeInBytes,
-    const int numChunks, const char** chunkFilePaths)
+    const struct keyNoncePair_secretbox* usePair, const char* outputFilePath,
+    const int headerSizeInBytes, const int numChunks, const char** chunkFilePaths)
 {
     // Initialize sodium
     if (sodium_init() < 0) {
@@ -55,5 +45,5 @@ int perform_dfs_decryptAndReconstructFile_secretbox(
     }
 
     return dfs_decryptAndReconstructFile_secretbox(outputFilePath, headerSizeInBytes,
-        numChunks, chunkFilePaths, usePair.key, usePair.nonce);
+        numChunks, chunkFilePaths, usePair->key, usePair->nonce);
 }
