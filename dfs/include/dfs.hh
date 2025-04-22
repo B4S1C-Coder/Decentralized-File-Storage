4a2c6a1c59ecc382c9d7ef3938aa145a9d67d095
B4S1C-Coder <saksham2005mittal@gmail.com>
2025-04-22 07:26:35 +0000

    Added dfs.hh & set storage-node-core to use dfs.hh

---
+++ dfs/include/dfs.hh
@@ -0,0 +1,15 @@
+#ifndef DFS_HH
+#define DFS_HH
+
+#include "chunk_ingestor.hh"
+#include "chunk_transmitter.hh"
+#include "chunk_metadata.hh"
+#include "fibonacci_heap.hh"
+#include "sequential_file_splitter.hh"
+#include "thread_pool.hh"
+#include "diagnostics.hh"
+#include "logger.hh"
+#include "stream_encrypt_decrypt.hh"
+#include "util.hh"
+
+#endif // DFS_HH
---
+++ storage-node-core/src/main.cc
@@ -0,0 +1,6 @@
+#include "dfs.hh"
+
+int main() {
+  fsn::logger::consoleLog("Hello, from storage-node-core");
+  return 0;
+}
